dfb094f1d03df925779aee2f36938ea0b87f3fb9
Takashi Nishimura <saishuheki.nisshing@gmail.com>
2023-12-06 19:03:00 +0900

    add: db接続処理の追加

---
+++ db/db.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"fmt"
+)
+
+func NewDB() *gorm.DB {
+	if os.Getenv("GO_ENV") == "dev" {
+		err := godotenv.Load()
+		if err != nil {
+			// エラーメッ性を出力し強制終了
+			log.Fatal(err)
+		}
+	}
+
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to database")
+	return db
+}
+
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+
+
